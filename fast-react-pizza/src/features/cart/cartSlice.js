a7fef1a405feffae2e845c52de0ec36a0b8afb7c
aniket30i <iamaniket.saha.09@protonmail.com>
2024-07-08 23:08:41 +0530

    cart redux

--- fast-react-pizza/src/features/cart/cartSlice.js
+++ fast-react-pizza/src/features/cart/cartSlice.js
@@ -1,3 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = {
-  cart: [],
+  cart: [
+    {
+      pizzaId: 12,
+      name: "Mediterranean",
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
 };
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      state.cart.push(action.payload);
+      //payload = newItem
+    },
+    deleteItem(state, action) {
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+      //payload=pizzaId
+    },
+    increaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.price = item.quantity * item.totalPrice;
+    },
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+      item.price = item.quantity * item.totalPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deletItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
